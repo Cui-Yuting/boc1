7b45be945c4f1cb92aebf021080fe67dcbab34ca
Cui-Yuting <1024790772@qq.com>
2020-07-31 09:48:28 +0800

    读取并插入选择题2

--- read2.py
+++ read2.py
@@ -3,13 +3,13 @@ import re
 import pymssql
 
 def ABCDtonum(sort):
-    if sort == 'A'|'a' :
+    if sort == 'A' or 'a' :
         return 1
-    elif sort == 'B'|'b' :
+    elif sort == 'B' or 'b' :
         return 2
-    elif sort == 'C'|'c':
+    elif sort == 'C' or 'c':
         return 3
-    elif sort == 'D'|'d':
+    elif sort == 'D' or 'd':
         return 4
     return
 
@@ -24,16 +24,16 @@ while p< len(lines):
 
     if re.match(r"\d",lines[p]):
         title = re.search(r"[^0-9.]+",lines[p]).group().rstrip()
-        sort = re.match(r"\d+",lines[p])
+        sort = re.match(r"\d+",lines[p]).group()
         sql = "insert into dbo.Vote2(title,sort,status,voting_show_result,pid,item_align,is_must_answer,min_select,max_select,creater_uid,copy_id) " \
               "values (%s,%d,0,0,0,0,0,0,0,0,0)"
         cursor.execute(sql,(title,sort))
         p=p+1
-        while lines[p]:#当选项不是空行
-            item_sort = ABCDtonum(re.match(r"[ABCDabcd]",lines[p]))
+        while p<len(lines)and lines[p]!='\n' :#当选项不是空行
+            item_sort = ABCDtonum(re.match(r"[ABCDabcd]",lines[p]).group())
             item_title = re.search(r"[^ABCDabcd.]+",lines[p]).group().rstrip()
-            item_sql = "insert into dbo.Vote_Item(vid,title,sort,creator_uid,status,parent_id,is_custom)" \
-                  "values(0,%s,%d,0,0,0,0)"
+            item_sql = "insert into dbo.Vote_Item(vid,title,sort,creator_uid,status,parent_id,is_custom) " \
+                  "values (0,%s,%d,0,0,0,0)"
             cursor.execute(item_sql,(item_title,item_sort))
             p = p+1
     p = p + 1
